78773c092fecb61635f4317897e374ffd5fc6cd9
Satyarthranjan <76532016+Satyarthranjan@users.noreply.github.com>
2024-03-14 00:19:19 +0530

    Create warshall.cpp

---
+++ warshall.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+void Warshall(vector<vector<int>>& graph) {
+    int n = graph.size();
+    vector<vector<int>> dist(n, vector<int>(n, 0));
+    for (int i = 0; i < n; ++i)
+        for (int j = 0; j < n; ++j)
+            dist[i][j] = graph[i][j];
+    for (int k = 0; k < n; ++k)
+        for (int i = 0; i < n; ++i)
+            for (int j = 0; j < n; ++j)
+                dist[i][j] = dist[i][j] || (dist[i][k] && dist[k][j]);
+    cout << "Transitive Closure Matrix:\n";
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            cout << dist[i][j] << "\t";
+        }
+        cout << endl;
+    }
+}
+int main() {
+    vector<vector<int>> graph = {
+        {1, 1, 0, 1},
+        {0, 1, 1, 0},
+        {0, 0, 1, 1},
+        {0, 0, 0, 1}
+    };
+    Warshall(graph);
+    return 0;
+}
