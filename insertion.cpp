f39a28cc144f0508098615ba252270f374f80211
Satyarthranjan <76532016+Satyarthranjan@users.noreply.github.com>
2024-03-14 00:38:28 +0530

    Update 1insertion.cpp

--- insertion.cpp
+++ insertion.cpp
@@ -6,10 +6,8 @@ void insertionSort(int arr[], int n)
 	{
 		int temp = arr[i];
 		int j = i - 1;
-
 		while (j >= 0 && arr[j] > temp) 
 		{
-			// arr[j + 1] = arr[j];
 			swap(arr[j + 1] ,arr[j]);
 			j --;             
 		}
